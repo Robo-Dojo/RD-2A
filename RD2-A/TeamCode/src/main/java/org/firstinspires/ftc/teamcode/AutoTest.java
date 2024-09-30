2504d7bb3e3e261556e2fc4dc1c150a74c1e9992
iulia2007 <craciunescuiulia2007@gmail.com>
2024-09-30 17:13:25 +0300

    autonomous test. 2.2seconds forward and 2 seconds to right

---
+++ RD2-A/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+
+//import com.acmerobotics.dashboard.config.Config;
+//import com.acmerobotics.roadrunner.Action;
+//import com.acmerobotics.roadrunner.Pose2d;
+//import com.acmerobotics.roadrunner.SequentialAction;
+//import com.acmerobotics.roadrunner.Vector2d;
+//import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+//@Config
+@Autonomous(name = "AutoTest", group = "Autonomous")
+public class AutoTest extends LinearOpMode {
+    private DcMotorEx leftFront, leftBack, rightBack, rightFront;
+
+
+    @Override
+    public void runOpMode() {
+        rightBack = hardwareMap.get(DcMotorEx.class, "rearRightMotor");
+        rightFront = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
+        leftFront = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
+        leftBack = hardwareMap.get(DcMotorEx.class, "rearLeftMotor");
+
+        waitForStart();
+        if(opModeIsActive()){
+            leftFront.setPower(-0.5);
+            leftBack.setPower(-0.5);
+            rightBack.setPower(0.5);
+            rightFront.setPower(0.5);
+            sleep(2200);
+
+            leftFront.setPower(-0.5);
+            leftBack.setPower(0.5);
+            rightBack.setPower(0.5);
+            rightFront.setPower(-0.5);
+            sleep(2000);
+
+            leftFront.setPower(0);
+            leftBack.setPower(0);
+            rightBack.setPower(0);
+            rightFront.setPower(0);
+
+            telemetry.update();
+        }
+
+
+        telemetry.update();
+        waitForStart();
+    }
+}
